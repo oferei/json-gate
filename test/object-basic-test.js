11f7ce6582d44fbce950735fd0744d74b0781579
Ofer Reichman <oferei@gmail.com>
2013-01-24 20:45:32 +0200

    added test to call validate with missing object.

---
+++ test/object-basic-test.js
@@ -0,0 +1,30 @@
+// this test is run by Vows (as all files matching *test.js)
+
+var vows = require('vows'),
+	should = require('should');
+
+var createSchema = require('..').createSchema,
+	config = require('./config');
+
+var schemaSimple = {
+	type: 'string'
+};
+
+vows.describe('Object Default').addBatch({
+	'when calling without an object': {
+		topic: function () {
+			var schema = createSchema(schemaSimple);
+			try {
+				schema.validate(undefined);
+				this.callback(null);
+			} catch(err) {
+				this.callback(err);
+			}
+		},
+		'we get an error': function (err, result) {
+			should.exist(err);
+			err.should.have.property('message', 'JSON object is undefined');
+			should.not.exist(result);
+		}
+	}
+}).export(module);
